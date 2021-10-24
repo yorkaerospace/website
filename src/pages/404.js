00c36e9cc8cacd128683c3676998353a8bbebc06
benslv <bensilverman10@gmail.com>
2021-10-24 17:13:44 +0100

    Set up base project

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>Sorry about that, we couldn't find this page.</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
